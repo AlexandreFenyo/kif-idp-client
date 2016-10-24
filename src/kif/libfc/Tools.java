fa6c55cd2b159eb7855710a7c05607e5df34a2b6
Alexandre Fenyo <fenyo@srv0.sec.le-rsi.net>
2016-10-24 17:38:45 +0200

    new

--- src/kif/libfc/Tools.java
+++ src/kif/libfc/Tools.java
@@ -1,24 +1,25 @@
+// A. Fenyo - 2016
+
 package kif.libfc;
 
-import java.util.Random;
-
-import org.bouncycastle.crypto.CipherParameters;
-import org.bouncycastle.crypto.DataLengthException;
-import org.bouncycastle.crypto.InvalidCipherTextException;
-import org.bouncycastle.crypto.engines.AESEngine;
-import org.bouncycastle.crypto.modes.CBCBlockCipher;
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.lang3.ArrayUtils;
-import org.bouncycastle.crypto.paddings.PKCS7Padding;
-import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
-import org.bouncycastle.crypto.params.KeyParameter;
-import org.bouncycastle.crypto.params.ParametersWithIV;
+import java.nio.charset.*;
+import java.util.*;
+import org.apache.commons.codec.*;
+import org.apache.commons.codec.binary.*;
+import org.apache.commons.lang3.*;
+import org.bouncycastle.crypto.*;
+import org.bouncycastle.crypto.engines.*;
+import org.bouncycastle.crypto.modes.*;
+import org.bouncycastle.crypto.paddings.*;
+import org.bouncycastle.crypto.params.*;
+import com.google.gson.*;
 
 public class Tools {
 	final private String KEY;
 	final private String IV;
 	private String nonce = null;
+	private String state = null;
+	private String json = null;
 
 	private void createNonce() {
 		final byte bytes[] = new byte[16];
@@ -35,13 +36,29 @@ public class Tools {
 		this.nonce = nonce;
 	}
 
+	public String getState() {
+		return state;
+	}
+
+	public void setState(final String state) {
+		this.state = state;
+	}
+
 	public Tools(final String KEY, final String IV) {
 		this.KEY = KEY;
 		this.IV = IV;
 	}
 
-	public String encode(final String url, final String state) throws DecoderException, DataLengthException, IllegalStateException, InvalidCipherTextException {
-		final String str = url + "?state=" + state + "&nonce=" + getNonce();
+	private String getJson() {
+		return json;
+	}
+
+	private void setJson(final String json) {
+		this.json = json;
+	}
+
+	public String encode(final String url) throws DecoderException, DataLengthException, IllegalStateException, InvalidCipherTextException {
+		final String str = url + "?nonce=" + getNonce() + "&state=" + getState();
 		final byte [] info_plaintext = str.getBytes();
         final PaddedBufferedBlockCipher aes = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESEngine()), new PKCS7Padding());
         final CipherParameters ivAndKey = new ParametersWithIV(new KeyParameter(Hex.decodeHex(KEY.toCharArray())), Hex.decodeHex(IV.toCharArray()));
@@ -53,5 +70,24 @@ public class Tools {
         final String info_ciphertext_hex = new String(Hex.encodeHex(info_ciphertext));
 		return info_ciphertext_hex;
 	}
+
+	public Identity decode(final String ciphertext_hex) throws DecoderException, DataLengthException, IllegalStateException, InvalidCipherTextException {
+		final byte [] ciphertext = Hex.decodeHex(ciphertext_hex.toCharArray());
+        final PaddedBufferedBlockCipher aes = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESEngine()), new PKCS7Padding());
+        final CipherParameters ivAndKey = new ParametersWithIV(new KeyParameter(Hex.decodeHex(KEY.toCharArray())), Hex.decodeHex(IV.toCharArray()));
+        aes.init(false, ivAndKey);
+        final int minSize = aes.getOutputSize(ciphertext.length);
+        final byte [] outBuf = new byte[minSize];
+        final int length1 = aes.processBytes(ciphertext, 0, ciphertext.length, outBuf, 0);
+        final int length2 = aes.doFinal(outBuf, length1);
+        json = new String(outBuf, 0, length1 + length2, Charset.forName("UTF-8"));
+        final Identity identity = new Gson().fromJson(json, Identity.class);
+		return identity;
+	}
 	
+	public boolean checkSecurity() {
+        final Gson gson = new Gson();
+        final Identity identity = gson.fromJson(getJson(), Identity.class);
+        return identity.getState().equals(getState()) && identity.getNonce().equals(getNonce());
+	}
 }
